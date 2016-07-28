6a3f45ff871b1cab42cab75f62efb59d174e2aa8
Mindaugas <mindaugas.kmita@gmail.com>
2016-07-28 17:49:49 +0000

    added custom post types implemented admin class responsible for admin functionality

---
+++ proposalpress.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Proposal Press
+ * Plugin URI:        http://proposalpress.com/
+ * Description:       Create professional Quotes & Proposals that clients will not resist to accept.
+ * Version:           1.0
+ * Author:            Function Themes
+ * Author URI:        https://www.functionthemes.com/
+ * Text Domain:       proposalpress
+ * Domain Path:       /languages
+ */
